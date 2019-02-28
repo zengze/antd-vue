2f73c0e0437eeb6602e77a487f4c56f8c3055a5b
DESKTOP-P3S29R5\ZengZe <zengze@tupu360.com>
2019-02-28 10:41:19 +0800

    添加hello world

--- src/main.js
+++ src/main.js
@@ -4,6 +4,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 路由跳转之前操作
+// router.beforeEach((to, from, next) => {
+//   console.log(to, 'to')
+//   console.log(from, 'from')
+//   next()
+// })
+
+// 路由跳转之后操作
+router.afterEach(route => {
+  if (route.meta.title) {
+    document.title = route.meta.title
+  }
+})
+
+// 关闭生产模式下给出的提示
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
